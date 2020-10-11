f68de51be14373b2b2f28434dc847d3ed489ed04
mprtcz <azet89"gmail.com>
2020-10-11 18:37:52 +0200

    Updates

---
+++ precache-manifest.ef7de3d4ff495910914262425ab01fec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d85c2922bea32fe38a7b89a4c77b5733",
+    "url": "/LearningReact/index.html"
+  },
+  {
+    "revision": "0091024661b79cac99f7",
+    "url": "/LearningReact/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/LearningReact/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LearningReact/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0091024661b79cac99f7",
+    "url": "/LearningReact/static/js/main.7de893ba.chunk.js"
+  },
+  {
+    "revision": "a3481825812c984ec695",
+    "url": "/LearningReact/static/js/runtime-main.ad855488.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/LearningReact/static/media/logo.ee7cd8ed.svg"
+  }
+]);
