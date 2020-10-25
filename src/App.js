540e870c38d5de51af41b5cde3e455aa0529327e
mprtcz <mpartacz@gmail.com>
2020-10-25 11:38:06 +0100

    Ŕ

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import AmountSpecifier from "./components/AmountSpecifier";
 function App() {
   return (
     <div className="App">
-      Ohei
       <br />
       <AmountSpecifier></AmountSpecifier>
     </div>
