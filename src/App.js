2648e0da80793bbd23d228c98205a53155cbe042
mprtcz <mpartacz@gmail.com>
2020-10-25 11:38:37 +0100

    Minor cleanup.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import AmountSpecifier from "./components/AmountSpecifier";
 function App() {
   return (
     <div className="App">
-      <br />
       <AmountSpecifier></AmountSpecifier>
     </div>
   );
