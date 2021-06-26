52a70c60ec6015400781a3d84732bb73453d72cd
mprtcz <michalpartacz@gmail.com>
2021-06-26 14:38:52 +0200

    Event handling for button and how arguments are passed into functs

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('hello ninjas!', e)
+    }
+
+    const handleClickAction = (name, e) => {
+        console.log('hello, ' + name + e.target);
+    }
+
     return ( 
     <div className="home">
         <h2>Homopage</h2>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={(e) => handleClickAction('Mario', e)}>Click me again</button>
     </div>
      );
 }
