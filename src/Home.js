41b1b3f9601ed79219a0aefaf030df88907cc6f9
mprtcz <michalpartacz@gmail.com>
2021-07-01 20:00:30 +0200

    Error catching and displaying.

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -11,18 +12,33 @@ const Home = () => {
   };
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsPending(false);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((response) => {
+          console.log(response);
+          // Checking if the response is 200.
+          if (!response.ok) {
+            throw new Error("Counld fetch the response");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setError(null);
+          setBlogs(data);
+          setIsPending(false);
+        })
+        // Catching connection errors.
+        .catch((error) => {
+          setIsPending(false);
+          console.log(error.message);
+          setError(error.message);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading</div>}
       {blogs && (
         <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
