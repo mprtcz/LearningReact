6e31f1494e3ee8e2051a260248d974f7e91824db
mprtcz <michalpartacz@gmail.com>
2021-07-01 19:51:35 +0200

    Added excluded conditional template loading.

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -16,11 +17,13 @@ const Home = () => {
       })
       .then((data) => {
         setBlogs(data);
+        setIsPending(false);
       });
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div>Loading</div>}
       {blogs && (
         <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
       )}
