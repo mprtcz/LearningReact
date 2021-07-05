eb97f74b11ff7c43c4d3d25ea200fd63dd57bddd
mprtcz <michalpartacz@gmail.com>
2021-07-05 20:02:04 +0200

    Added post requesting to the form

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,21 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-    console.log(blog);
+
+    setIsPending(true);
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("New blog added");
+      setIsPending(false);
+    });
   };
 
   return (
@@ -33,7 +43,8 @@ const Create = () => {
           <option value="mario">Mario</option>
           <option value="yoshi">Yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {isPending && <button disabled>Adding blog...</button>}
+        {!isPending && <button>Add Blog</button>}
         <p>{title}</p>
         <p>{body}</p>
         <p>{author}</p>
